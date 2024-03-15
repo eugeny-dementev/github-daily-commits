e330d77d74b0fd523b2053c447cd8b4382e678bb
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-03-15 20:44:37 +0800

    rework colors per commit count

--- check_commits.sh
+++ check_commits.sh
@@ -11,16 +11,18 @@ number=$(echo "$first_line" | awk '{print $1}')
 
 # Check the value and send different strings accordingly
 if [ "$number" -lt 1 ]; then
-    echo -n "black" | nc -4u -w0 localhost 1738
+    echo -n "hollow" | nc -4u -w0 localhost 1738
 elif [ "$number" -lt 3 ]; then
-    echo -n "cyan" | nc -4u -w0 localhost 1738
-elif [ "$number" -lt 7 ]; then
     echo -n "green" | nc -4u -w0 localhost 1738
+elif [ "$number" -lt 7 ]; then
+    echo -n "cyan" | nc -4u -w0 localhost 1738
 elif [ "$number" -lt 10 ]; then
     echo -n "yellow" | nc -4u -w0 localhost 1738
-elif [ "$number" -lt 15 ]; then
+elif [ "$number" -lt 13 ]; then
     echo -n "red" | nc -4u -w0 localhost 1738
-elif [ "$number" -lt 20 ]; then
+elif [ "$number" -lt 15 ]; then
+    echo -n "black" | nc -4u -w0 localhost 1738
+elif [ "$number" -lt 18 ]; then
     echo -n "question" | nc -4u -w0 localhost 1738
 else
     echo -n "exclamation" | nc -4u -w0 localhost 1738
